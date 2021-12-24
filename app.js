b8176b644385dff506cb0beb3ec9c060886ec551
smarinan90 <smarinan90@gmail.com>
2021-12-23 20:10:54 -0500

    [ADD] Project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en puerto 3000"));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
+app.post('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
